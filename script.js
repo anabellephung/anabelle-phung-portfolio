bf0b742e5550d13145dcbe2773741e3f3348a56c
Your <yourname@your-email.com>
2020-10-13 02:29:38 -0400

    updated html and css

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 $(function() {
   const $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    if ($hamburger.toggleClass("is-active")) {
-      $('ul').slideToggle(e)
-    } else {
-      $('ul').hide(e)
-    }
+  $hamburger.on("click", function() {
+    $hamburger.toggleClass("is-active")
+    $('.menuList').slideToggle();
   });
 })
+
