cd854b280c02d05690126554d92e72573af8a9dc
Your <yourname@your-email.com>
2020-10-21 12:21:33 -0400

    animation word change

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
 $(function() {
-  // var newHash = "",
-  //   $mainContent = $("#main-content"),
-  //   $pageWrap = $("#page-wrap"),
-  //   baseHeight = 0,
-  //   $el;
+  let words = [
+    'developer',
+    'designer',
+    'female',
+    'collaborator',
+  ], i = 0; 
 
-  // $pageWrap.height($pageWrap.height());
-  // baseHeight = $pageWrap.height() - $mainContent.height();
+  setInterval(function () {
 
-  // $("nav").delegate("a", "click", function () {
-  //   window.location.hash = $(this).attr("href");
-  //   return false;
-  // });
+    $('#change').fadeOut(function () {
+      $(this).html(words[i = (i + 1) % words.length]).fadeIn(); 
+    });
+  }, 2000);
 
-  // $(window).bind('hashchange', function () {
-
-  //   newHash = window.location.hash.substring(1);
-
-  //   if (newHash) {
-  //     $mainContent
-  //       .find("#guts")
-  //       .fadeOut(200, function () {
-  //         $mainContent.hide().load(newHash + " #guts", function () {
-  //           $mainContent.fadeIn(200, function () {
-  //             $pageWrap.animate({
-  //               height: baseHeight + $mainContent.height() + "px"
-  //             });
-  //           });
-  //           $("nav a").removeClass("current");
-  //           $("nav a[href='" + newHash + "']").addClass("current");
-  //         });
-  //       });
-  //   };
-
-  // });
-
-  // $(window).trigger('hashchange');
-
-
-
-  
   // Toggle hamburger mobile menu
   const $hamburger = $(".hamburger");
   $hamburger.on("click", function() {
